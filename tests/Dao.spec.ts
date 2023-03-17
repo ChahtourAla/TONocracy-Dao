4dd60bdd7ca8768f334100df706f1901a13dd283
issamunic <89040136+issamunic@users.noreply.github.com>
2023-03-17 13:59:40 +0100

    done

--- tests/Dao.spec.ts
+++ tests/Dao.spec.ts
@@ -1,8 +1,31 @@
+import { DaoConfig } from './../wrappers/Dao';
 import { Blockchain, SandboxContract } from '@ton-community/sandbox';
-import { Cell, toNano } from 'ton-core';
+import { Cell, toNano, Dictionary, DictionaryValue } from 'ton-core';
 import { Dao } from '../wrappers/Dao';
 import '@ton-community/test-utils';
 import { compile } from '@ton-community/blueprint';
+
+const ListValue: DictionaryValue<string> = {
+    serialize(src: string, builder) {
+        builder.storeStringRefTail(src);
+    },
+    parse(src) {
+        return src.loadStringRefTail();
+    },
+};
+
+let membersDict: Dictionary<number, string> = Dictionary.empty(Dictionary.Keys.Uint(256), ListValue);
+let proposalDict: Dictionary<number, string> = Dictionary.empty(Dictionary.Keys.Uint(256));
+
+membersDict.set(1, 'kQCwlqJG250ThRWZSet1IWZoXTOA3kgBH9pSWhKp2OwXRfFZ');
+
+let daoConfig = {
+    dao_name: 'dao test name',
+    dao_purpose: 'dao test purpose',
+    creation_time: Date.now(),
+    members: membersDict,
+    proposals_list: proposalDict,
+};
 
 describe('Dao', () => {
     let code: Cell;
@@ -17,7 +40,7 @@ describe('Dao', () => {
     beforeEach(async () => {
         blockchain = await Blockchain.create();
 
-        dao = blockchain.openContract(Dao.createFromConfig({}, code));
+        dao = blockchain.openContract(Dao.createFromConfig(daoConfig, code));
 
         const deployer = await blockchain.treasury('deployer');
 
@@ -30,8 +53,17 @@ describe('Dao', () => {
         });
     });
 
-    it('should deploy', async () => {
-        // the check is done inside beforeEach
-        // blockchain and dao are ready to use
+    it('get dao name', async () => {
+        let name = await dao.getDaoName();
+        expect(name).toEqual('dao test name');
+    });
+
+    it('get dao members', async () => {
+        let members = await dao.getMembersList();
+        let slice = members.beginParse();
+        let list = slice.loadDictDirect(Dictionary.Keys.Uint(256), ListValue).values();
+        slice.endParse();
+
+        console.log(list);
     });
 });
